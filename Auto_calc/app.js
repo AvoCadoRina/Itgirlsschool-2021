07c16d9d84c3930d63d4752d31c2befad182a7b8
Ekaterina <ms.holycats@mail.ru>
2022-02-08 23:00:40 +0300

    fixed js

--- Auto_calc/app.js
+++ Auto_calc/app.js
@@ -1,39 +1,25 @@
-var btn = document.querySelector('#btn'),
-    price_old = document.querySelector('#price_old').innerHTML = 500;
-volume = document.querySelector('#volume').innerHTML = 500;
-capacity = document.querySelector('#capacity').innerHTML = 500;
-
-kg = 5.5,
-    kub = 3500,
-    km = 2;
-// range slider
-range.onchange = function() {
-        var price_old = document.querySelector('#price_old').innerHTML = range.value;
-        var volume = document.querySelector('#volume').innerHTML = range.value;
-        var capacity = document.querySelector('#capacity').innerHTML = range.value;
+function raschitat() {
+    let values_all = 0;
+    let radios = document.querySelectorAll('input[type="radio"]');
+    for (let radio of radios) {
+        if (radio.checked) {
+            values_all += Number(radio.value);
+        }
     }
-    // Basic function  
-btn.onclick = function() {
-    if (weight.value != '' && vol.value != '') {
-        if (load.checked) {
-            load.value = 1500;
-        } else {
-            load.value = 0;
-        }
-        if (del.checked) {
-            del.value = 3500;
-        } else {
-            del.value = 0;
-        }
-        if (frag.checked) {
-            frag.value = 2000;
-        } else {
-            frag.value = 0;
-        }
-
-        var sum = price_old + (vol.value * kub) + Number(load.value) + +Number(del.value) + Number(frag.value) + (range.value * km);
-        out.innerHTML = sum;
+    price_old = document.getElementById('price_old').value;
+    volume = document.getElementById('volume').value;
+    volume_cost = volume * 1.2;
+    capacity = document.getElementById('capacity').value;
+    capacity_cost = capacity * 4;
+    if (price_old == "") {
+        alert("Вы не указали цену на аукционе");
+    } else if (volume == "") {
+        alert("Вы не указали объем двигателя");
+    } else if (capacity == "") {
+        alert("Вы не указали мощность");
     } else {
-        alert('Введите все данные');
+        stoimost = Number(price_old) + Number(volume_cost) + Number(capacity_cost) + Number(values_all);
+        value_fin = Math.ceil(stoimost);
+        alert("Стоимость равна: " + value_fin + " $");
     }
 }
