32d19f1e55c5d990b43cad24b3c8000e2d1afa4d
Ekaterina <ms.holycats@mail.ru>
2022-02-12 20:27:11 +0300

    done

---
+++ number_generation.js
@@ -0,0 +1,25 @@
+var results = [];
+for (var i = 0; i < 10; i++) {
+    var random = Math.floor((Math.random() * 20) - 10);
+    results.push(random);
+}
+let numbers = results.join();
+let max = Math.max(...results);
+let min = Math.min(...results);
+let sum = 0;
+
+function averageСost(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+let average = averageСost(results);
+let multiply = results.reduce((acc, rec) => acc * rec);
+
+alert(`Массив: ${(results)}`)
+alert(`Максимальное: ${(max)}`)
+alert(`Минимальное: ${(min)}`)
+alert(`Среднее арифметическое: ${(average)}`)
+alert(`Сумма: ${(sum)}`)
+alert(`Произведение: ${(multiply)}`)
