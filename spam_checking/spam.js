d900224de2dc2b4c5d9f2b3ae4ed20ed311d042c
Ekaterina <ms.holycats@mail.ru>
2022-02-19 10:17:20 +0300

    done task 3

--- spam_checking/spam.js
+++ spam_checking/spam.js
@@ -1,12 +1,34 @@
 function checkSpam() {
     let strings = [];
+    let fotos = [];
     const div = document.getElementById('div1');
+    const timeright = document.getElementById('time-right');
     let newStr = "";
     let str = document.getElementById("str").value;
     let newstring = str.toString();
     let lowerStr = newstring.toLowerCase();
+    let data_user = document.getElementById("user").value;
     x = lowerStr.replace(/viagra/g,  "***");
     y = x.replace(/xxx/g,  "***");
-    strings.push(y);
+    z = y + '' + '"send by ' + data_user + '"';
+    strings.push(z);
+    var now = new Date();
+    timeright.innerHTML += now.toLocaleTimeString() + '<br>';
     div.innerHTML += strings + '<br>';
+    var preview = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    };
+    if (saving.checked) {
+        localStorage.setItem(data_user, file);
+    }
 }
