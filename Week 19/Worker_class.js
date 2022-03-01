0d0a1a687bac2ea754d1e3b67ebb5ee46a5e9d15
Ekaterina <ms.holycats@mail.ru>
2022-03-01 15:20:37 +0300

    done tasks 1 and 2

---
+++ Week 19/Worker_class.js
@@ -0,0 +1,13 @@
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+    getSalary() {
+        return (this.rate * this.days);
+    }
+}
+
+var worker = new Worker('Иван', 'Иванов', 10, 31);
--- anketa_kot/app.js
+++ anketa_kot/app.js
@@ -15,4 +15,45 @@ function checkOrNot() {
         button.removeAttribute('disabled');
     };
 }
-button.addEventListener('click', checkOrNot);
+
+
+
+class Cat {
+    constructor(surname, contacts, petname, poroda, food, comment, sex) {
+        this.surname = surname;
+        this.contacts = contacts;
+        this.petname = petname;
+        this.poroda = poroda;
+        this.food = food;
+        this.comment = comment;
+        this.sex = sex;
+    }
+    CatData() {
+        alert(`Владелец ${this.surname} Контакты ${this.contacts} Комментарий ${this.comment} Имя питомца ${this.petname} пол ${this.sex} порода ${this.poroda} предпочитаемая еда: ${this.food}`);
+    }
+};
+
+function ShowData() {
+    var userSurname = document.getElementById('surname').value;
+    var userContacts = document.getElementById('contacts').value;
+    var UserPetname = document.getElementById('name').value;
+    var Porodanew = document.getElementById("poroda");
+    var PetPoroda = Porodanew.options[Porodanew.selectedIndex].text;
+    var PetFood = document.querySelectorAll('input.checkbox:checked').value;
+    var UserComment = document.getElementById('UserComment').value;
+    var Petsex = document.querySelector('input[name="sex"]:checked').value;
+    var all_checkbox = document.getElementsByName("food");
+    var PetFood = "";
+    for (var i = 0; i < all_checkbox.length; i++) {
+        if (all_checkbox[i].checked) { PetFood += all_checkbox[i].value + " "; }
+    };
+    let Cat1 = new Cat(userSurname, userContacts, UserPetname, PetPoroda, PetFood, UserComment, Petsex);
+    Cat1.CatData();
+};
+
+
+
+button.addEventListener('click', () => {
+    checkOrNot();
+    ShowData();
+});
